6a619ae89b1c471b067bdcb95a35a80f54873dec
hansen1416 <badapplesweetie@gmail.com>
2022-12-04 17:32:27 +0800

    1

---
+++ poly_regression.py
@@ -0,0 +1,61 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+
+
+figsizes = np.array([[i*i] for i in range(1, 16)])
+marksizes = [0.5, 2, 4.2, 8, 12, 18, 24, 32, 40, 50, 60, 72, 84, 96, 110]
+
+
+def plot_result(X, y, y_pred, poly_reg, lin_reg, degree):
+
+    X_grid = np.arange(min(X), max(X), 0.1)
+    X_grid = X_grid.reshape((len(X_grid), 1))
+
+    plt.scatter(X, y, color='red')
+    plt.scatter(X, y_pred, color='green')
+
+    plt.plot(X_grid, lin_reg.predict(
+        poly_reg.fit_transform(X_grid)), color='black')
+
+    plt.title('Polynomial Regression')
+    plt.xlabel('Figsize')
+    plt.ylabel('Mark size')
+
+    plt.show()
+
+    plt.savefig(os.path.join('regression_result', 'result_degree_{}.jpg'.format(degree)),
+                dpi=200, format='jpg')
+
+
+if __name__ == "__main__":
+
+    degree = 1
+
+    poly_reg = PolynomialFeatures(degree=degree)
+    X_poly = poly_reg.fit_transform(figsizes)
+
+    lin_reg = LinearRegression()
+    lin_reg.fit(X_poly, marksizes)
+
+    y_pred = lin_reg.predict(X_poly)
+
+    plot_result(figsizes, marksizes, y_pred, poly_reg, lin_reg, degree)
+
+    # grid2D = {
+    #     "j0": 1751622.5000000084,
+    #     "k0": 5947162.5000000652,
+    #     "numJ": 273,
+    #     "numK": 189,
+    #     "deltaJ": 5.0,
+    #     "deltaK": 5.0
+    # }
+
+    # x_test = poly_reg.fit_transform(np.array([[grid2D['numJ']/i * grid2D['numK']/i]
+    #                                           for i in [5, 10, 20]]))
+
+    # y_test_pred = lin_reg.predict(x_test)
+
+    # print(y_test_pred)
---
+++ simpleplot.py
@@ -0,0 +1,93 @@
+from io import BytesIO
+import os
+from random import choices
+from string import ascii_uppercase, digits
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+from main import get_flooding_waterlevel_cmap
+
+
+def random_string(k=6):
+
+    population = ascii_uppercase + digits
+    return str.join('', choices(population, k=k))
+
+
+def random_data(width=72, height=72):
+
+    np.random.seed(42)
+
+    data = np.array(np.meshgrid(list(range(width)),
+                    list(range(height)))).T.reshape(-1, 2)
+
+    data = np.append(data, np.random.rand(
+        width*height).T.reshape(-1, 1), axis=1)
+
+    return data
+
+
+def simple_scatter_plot(data, figsize=(1, 1), s=0.2):
+
+    plt.figure(figsize=figsize)
+
+    # plt.axis('off')
+    plt.tight_layout()
+
+    x = np.array(data[:, 0], dtype=int)
+    y = np.array(data[:, 1], dtype=int)
+
+    plt.scatter(x, y, c=data[:, 2],
+                alpha=1, edgecolors='none', linewidths=0, marker='s',
+                s=s,
+                cmap=get_flooding_waterlevel_cmap(), vmin=0, vmax=1)
+
+    img_buf = BytesIO()
+
+    plt.savefig(img_buf, transparent=True, dpi=200,
+                format='png')  # , pad_inches=0)
+
+    plt.clf()
+    plt.close()
+
+    img_buf.seek(0)
+
+    img = Image.open(img_buf)
+
+    # Cropped image of above dimension
+    # (It will not change original image)
+    # img = img.crop(self.crop_box[img_size])
+
+    alpha_channel = img.getchannel('A')
+
+    # Make all opaque pixels into semi-opaque
+    alpha_channel = alpha_channel.point(
+        lambda i: 128 if i > 0 else 0)
+
+    img.putalpha(alpha_channel)
+
+    img.save(os.path.join('simpleplot', "simple_plot_{}_{}.png".format(
+        str(figsize), str(s))), format='PNG')
+
+
+if __name__ == "__main__":
+
+    data = random_data()
+
+    print(data)
+    print(data.shape)
+
+    # muids = np.load(os.path.join('tmp', 'muids_3857.npy'))
+    # data = np.load(os.path.join('tmp', '1666857600.npy'))
+
+    # print(muids.shape)
+    # print(data.shape)
+
+    fig = list(range(1, 16))
+    s = [0.5, 2, 4.2, 8, 12, 18, 24, 32, 40, 50, 60, 72, 84, 96, 110]
+
+    for i, f in enumerate(fig):
+
+        simple_scatter_plot(data, figsize=(f, f), s=s[i])
